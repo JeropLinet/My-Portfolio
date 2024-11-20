3168870f4d1045b822fb67a20b4e5efdbd0d26f2
Linet <teweilinet@gmail.com>
2024-11-20 06:04:18 +0300

    add host command

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
+/* eslint-env node */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 3000
+  }
 })
