20d890067d248d4b942f8b7c707f09085962d4ec
Linet <teweilinet@gmail.com>
2024-11-20 05:25:32 +0300

    create layout

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>Portfolio © Alaina Thomas Digital Marketing</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+function Navbar() {
+    return (
+        <>
+        <div className="navbar">
+         <h1 >Alaina Thomas</h1>
+        </div>
+        <div className="space"></div>
+        </>
+    );
+}
+
+export default Navbar;
